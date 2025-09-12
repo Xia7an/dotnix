c5cd10d4551e6bd192f03353d7ac1e6cd26bb2fc
inoyu <inoyu0329@gmail.com>
2025-09-12 14:54:40 +0000

    fix: ollama default model

--- module/NixOS/ollama.nix
+++ module/NixOS/ollama.nix
@@ -7,6 +7,5 @@
       host = "0.0.0.0";
       openFirewall = true;
       # Optional: preload models, see https://ollama.com/library
-      loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
     };
 }
