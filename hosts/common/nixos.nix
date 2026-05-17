3bf951fe4c4d6f1cad25f3c0dbf971babeb3a62e
Inoyu <117442042+Xia7an@users.noreply.github.com>
2026-05-17 21:50:03 +0900

    feat: バイナリキャッシュを使うように変更

--- hosts/common/nixos.nix
+++ hosts/common/nixos.nix
@@ -89,9 +89,10 @@
 
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    substituters          = [ "https://hyprland.cachix.org" ];
-    trusted-substituters  = [ "https://hyprland.cachix.org" ];
+    substituters          = [ "https://cache.nixos.org/" "https://hyprland.cachix.org" ];
+    trusted-substituters  = [ "https://cache.nixos.org/" "https://hyprland.cachix.org" ];
     trusted-public-keys   = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
