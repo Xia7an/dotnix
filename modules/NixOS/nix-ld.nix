84a118d0f1a92874d2e85c2ea6b5366755203cdd
Inoyu <117442042+Xia7an@users.noreply.github.com>
2026-05-25 22:05:55 +0900

    fix: インデント

--- modules/NixOS/nix-ld.nix
+++ modules/NixOS/nix-ld.nix
@@ -13,36 +13,33 @@
     xz
     sqlite
     readline
-    # --- 今回のエラー（libgobject-2.0.so.0）を解決するパッケージ ---
-  glib
-
-  # --- 今後Electron/Chromiumで高確率で必要になる主要ライブラリ ---
-  nss
-  nspr
-  atk
-  at-spi2-atk
-  cups
-  dbus
-  libdrm
-  gdk-pixbuf
-  gtk3
-  pango
-  cairo
-  xorg.libX11
-  xorg.libXcomposite
-  xorg.libXdamage
-  xorg.libXext
-  xorg.libXfixes
-  xorg.libXrandr
-  xorg.libXrender
-  xorg.libXtst
-  xorg.libXxi
-  xorg.libXScrnSaver
-  xorg.libxcb
-  libxkbcommon
-  mesa
-  alsa-lib
-  expat
-  udev
+    glib 
+    nss
+    nspr
+    atk
+    at-spi2-atk
+    cups
+    dbus
+    libdrm
+    gdk-pixbuf
+    gtk3
+    pango
+    cairo
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libXxi
+    xorg.libXScrnSaver
+    xorg.libxcb
+    libxkbcommon
+    mesa
+    alsa-lib
+    expat
+    udev
   ];
 }
