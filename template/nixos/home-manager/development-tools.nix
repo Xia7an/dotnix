e7acea04ceddaf880c80137806289224aee7537e
Inoyu <117442042+Xia7an@users.noreply.github.com>
2026-09-17 14:58:24 +0900

    fix: texを削除

--- template/nixos/home-manager/development-tools.nix
+++ template/nixos/home-manager/development-tools.nix
@@ -4,13 +4,8 @@
     ../../../modules/Home/development/direnv.nix
     ../../../modules/Home/development/rust.nix
     ../../../modules/Home/development/cpp.nix
-    ../../../modules/Home/development/go.nix
-    ../../../modules/Home/development/zig.nix
     ../../../modules/Home/development/js-ts.nix
     ../../../modules/Home/development/python.nix
-    ../../../modules/Home/development/java-dotnet.nix
-    ../../../modules/Home/development/database.nix
-    ../../../modules/Home/development/tex.nix
     ../../../modules/Home/virtualization/docker.nix
   ];
 }
